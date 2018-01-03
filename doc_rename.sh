abc33f0c9c2c807fcf3702f2ddb53c885f7ca6d4
terentev-mn <terentev.mn@gmail.com>
2018-01-03 21:00:17 +0300

    add new file

--- doc_rename.sh
+++ doc_rename.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
- 
+
 IFS_old=$IFS
 IFS=''
-if [ -z $1 ];then echo Give target directory; exit 0;fi
- 
+
+if [ $# -ne 1 ]; then
+        echo "Enter directory"
+    	exit 0;
+fi
+
 find "$1" -depth -name '*' | while read file ; do
-        directory=$(dirname "$file")
-        oldfilename=$(basename "$file")
-        newfilename=$(echo "$oldfilename" |tr ',' '.' |sed 's/\.\./\./g'|sed 's/\ \ /\ /g'|sed 's/__/_/g'|sed 's/\"//g'|sed 's/\ \./\./g')
+        directory=$(dirname $file)
+        oldfilename=$(basename $file)
+        newfilename=$(echo $oldfilename | tr -s [:punct:]| tr -s [:blank:]| sed 's/\ \./\./g'| sed 's/\"//g')
         if [ "$oldfilename" != "$newfilename" ]; then
                 # Renaming
                 mv -v "$directory/$oldfilename" "$directory/$newfilename"
-                # Printing 
-#                echo ""$directory/$oldfilename" ---> "$directory/$newfilename""
+                # Printing
+                 echo ""$directory/$oldfilename" ---> "$directory/$newfilename""
         fi
         done
 exit 0
